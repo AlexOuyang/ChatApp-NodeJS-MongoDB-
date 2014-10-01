2694bbf205af0fbbf0a403b5fbda4952cfcbbc80
chenxing ouyang <ouyangchenxing@hotmail.com>
2014-09-30 19:41:24 -0700

    add status updating ability

--- README.md
+++ README.md
@@ -14,7 +14,13 @@ Mongodb
 3. switch to chat: use chat
 =======
 to check chat data in mongodb: db.messages.find()
-to remote chat data: db.messages.remove()
+to remote chat data: db.messages.remove({})
+
+
+How to test in Chrome developer console
+==========
+1. listen to port:  var socket = io.connect('http://127.0.0.1:8080');
+2. insert data: socket.emit('input',{name:"Alex","message":"hello"});
 
 
 
--- server.js
+++ server.js
@@ -9,6 +9,10 @@ mongo.connect('mongodb://127.0.0.1/chat', function(err,db){
 	client.on('connection', function(socket){
 		//create a database collecction
 		var col = db.collection('messages');
+		var sendStatus = function(s){
+			//emit the status to the client
+			socket.emit('status', s);
+		};
 		//wait for incoming connection 'input'
 		socket.on('input', function(data){
 			//grab the "name" and "message" from data
@@ -18,11 +22,15 @@ mongo.connect('mongodb://127.0.0.1/chat', function(err,db){
 			var whitespacePattern = /^\s*$/;
 			//if there is a match of whitespace in the 'name' or 'message' then its invalid
 			if(whitespacePattern.test(name) || whitespacePattern.test(message)){
-				console.log('Invalid');
+				//console.log('Invalid data, can't insert into the database);
+				//validate the status on serverside so it can't be changed by the users
+				sendStatus('Name and message is required.');
 			} else {
 				// if the name or message is valid, insert the data into the database collection
 				col.insert({name:name, message:message}, function(){
-					console.log('inserted');
+					//console.log('Data inserted');
+					//notify the client to clear the textarea
+					sendStatus({message:"Message sent", clear:true});
 				});
 			}
 
