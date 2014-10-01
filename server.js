2dda0204513af61f428ba19702a8fed7d146fc02
chenxing ouyang <ouyangchenxing@hotmail.com>
2014-09-30 21:43:24 -0700

    updating messages done

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var mongo = require('mongodb').MongoClient;
 var client = require('socket.io').listen(8080).sockets;
 
 mongo.connect('mongodb://127.0.0.1/chat', function(err,db){
-	if (err)
-		throw err;
+	if (err) throw err;
 
 	//socket.io listens for connection
 	client.on('connection', function(socket){
@@ -13,6 +12,15 @@ mongo.connect('mongodb://127.0.0.1/chat', function(err,db){
 			//emit the status to the client
 			socket.emit('status', s);
 		};
+
+		//emit all messages in textarea
+		//limit the amount of messages we want to sow in textarea, 
+		//sort({_id:1}) sorts the data in descending order by id
+		col.find().limit(100).sort({_id: 1}).toArray(function(err, res){
+			if(err) throw err;
+			socket.emit('output', res);
+		});
+
 		//wait for incoming connection 'input'
 		socket.on('input', function(data){
 			//grab the "name" and "message" from data
@@ -20,6 +28,7 @@ mongo.connect('mongodb://127.0.0.1/chat', function(err,db){
 			var message = data.message;
 			//whitespace regular expression
 			var whitespacePattern = /^\s*$/;
+
 			//if there is a match of whitespace in the 'name' or 'message' then its invalid
 			if(whitespacePattern.test(name) || whitespacePattern.test(message)){
 				//console.log('Invalid data, can't insert into the database);
@@ -29,7 +38,10 @@ mongo.connect('mongodb://127.0.0.1/chat', function(err,db){
 				// if the name or message is valid, insert the data into the database collection
 				col.insert({name:name, message:message}, function(){
 					//console.log('Data inserted');
-					//notify the client to clear the textarea
+
+					//Emit the latest messsage to all clients
+					client.emit('output', [data]);
+					//notify the client to clear the input textarea
 					sendStatus({message:"Message sent", clear:true});
 				});
 			}
